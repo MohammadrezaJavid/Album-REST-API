9bb2b92167388a0d70db672c08ee1b2dc6d1b808
mohammadrezajavid <javid.9@yahoo.com>
2023-07-15 16:30:15 +0330

    update main.go

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"album/api"
+	"album/config"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	config.LoadEnv()
 	router := gin.Default()
 	api.Routes(router)
 	router.Run(":8070")
