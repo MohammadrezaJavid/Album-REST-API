ee59177b6cca39b8ef39841c3514d9f84e7d8848
mohammadrezajavid <javid.9@yahoo.com>
2023-07-22 18:37:26 +0330

    update router.go file

--- api/router.go
+++ api/router.go
@@ -15,7 +15,7 @@ func Routes() *gin.Engine {
 	{
 		api.GET("/albums", handlers.GetAlbums)          // Get all album
 		api.POST("/albums", handlers.PostAlbum)         // Add one album
-		api.POST("/albums", handlers.UpdateAlbumByID)   // Update one album by ID
+		api.PUT("/albums", handlers.UpdateAlbumByID)    // Update one album by ID
 		api.GET("/albums/:id", handlers.GetAlbumByID)   // Get one album by ID
 		api.DELETE("/albums/:id", handlers.DeleteAlbum) // Delete one album by ID
 		api.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
