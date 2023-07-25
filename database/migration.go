70f011e952f7bb10c891c81a183d264b9a941c9c
mohammadrezajavid <javid.9@yahoo.com>
2023-07-25 18:49:22 +0330

    update migration.go file

--- database/migration.go
+++ database/migration.go
@@ -3,24 +3,28 @@ package database
 import (
 	"album/database/models"
 	"album/database/repositories"
+	"errors"
 	"log"
 )
 
 func Migrate() {
 
 	if err := repositories.DataBase.AutoMigrate(&models.Album{}); err != nil {
-		log.Fatal(err.Error())
+		log.Fatal(errors.New(err.Error()))
 	}
 
 	if err := repositories.DataBase.AutoMigrate(&models.User{}); err != nil {
-		log.Fatal(err.Error())
+		log.Fatal(errors.New(err.Error()))
 	}
 }
 
-func MigrateDown() error {
-	err := repositories.DataBase.Migrator().DropTable(&models.Album{})
-	if err != nil {
-		return err
+func MigrateDown() {
+
+	if err := repositories.DataBase.Migrator().DropTable(&models.Album{}); err != nil {
+		log.Fatal(errors.New(err.Error()))
 	}
-	return nil
+
+	if err := repositories.DataBase.Migrator().DropTable(&models.User{}); err != nil {
+		log.Fatal(errors.New(err.Error()))
+	}
 }
