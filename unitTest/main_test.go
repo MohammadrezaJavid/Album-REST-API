b44cf944f77160230fa91d7eec2cd85a9fd1a458
mohammadrezajavid <javid.9@yahoo.com>
2023-07-25 18:50:02 +0330

    update main_test file

--- unitTest/main_test.go
+++ unitTest/main_test.go
@@ -1,30 +1,61 @@
 package unittest
 
 import (
+	"album/api"
+	"album/config"
+	"album/database"
+	"album/database/repositories"
+	"bytes"
+	"encoding/json"
+	"errors"
+	"log"
+	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 
 	"github.com/gin-gonic/gin"
 )
 
 func TestMain(m *testing.M) {
-	//TODO
+	gin.SetMode(gin.TestMode)
+
+	setUpMigrate()
+	resutlCode := m.Run()
+	downMigrate()
+
+	os.Exit(resutlCode)
 }
 
 func setUpMigrate() {
-	//TODO
+	config.LoadEnv()
+
+	repositories.DatabaseHandle()
+	database.Migrate()
 }
 
 func downMigrate() {
-	//TODO
+	database.MigrateDown()
 }
 
-func route() *gin.Engine {
-	//TODO
-	return nil
+func getRoutes() *gin.Engine {
+	return api.Routes()
 }
 
 func makeRequest(httpMethod, url string, body interface{}, isAuthRequest bool) (responseWriter *httptest.ResponseRecorder) {
-	//TODO
-	return nil
+
+	requestBody, _ := json.Marshal(body)
+	request, err := http.NewRequest(
+		httpMethod,
+		url,
+		bytes.NewBuffer(requestBody),
+	)
+	if err != nil {
+		log.Fatal(errors.New(err.Error()))
+	}
+
+	responseWriter = httptest.NewRecorder()
+	routes := getRoutes()
+	routes.ServeHTTP(responseWriter, request)
+	return
 }
