49431b5202c1ddb1f2156462b096de1f1d8a2e36
mohammadrezajavid <javid.9@yahoo.com>
2023-08-04 21:00:45 +0330

    update main file

--- main.go
+++ main.go
@@ -6,12 +6,13 @@ import (
 	"album/database"
 	"album/database/repositories"
 	"fmt"
+	"os"
 
 	_ "album/docs"
 )
 
 func init() {
-	fmt.Println(">>Initializing config...<<")
+	fmt.Println(">>Initializing config<<")
 	config.LoadEnv()
 	repositories.DatabaseHandle()
 	database.Migrate()
@@ -22,7 +23,7 @@ func init() {
 * https://github.com/swaggo/gin-swagger/issues/90#issuecomment-903813846
 **/
 
-// @title Go + Gin Album rest API Service
+// @title Go + Gin Album Rest API Service
 // @version 1.0
 // @description This is a sample server for save Albums in mysql database
 
@@ -40,8 +41,8 @@ func init() {
 // @securityDefinitions.apikey BearerAuth
 // @in header
 // @name Authorization
-
 func main() {
+	var apiHost string = os.Getenv("API_HOST")
 	router := api.Routes()
-	router.Run(":8070")
+	router.Run(apiHost + ":8070")
 }
